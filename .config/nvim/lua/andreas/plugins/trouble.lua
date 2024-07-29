7a6cbf20406d3cd43cb3535cce75abf8386402b1
Andreas Wågø Wilson <andreasw@gmail.com>
2024-07-29 19:56:57 +0200

    fix trouble

--- .config/nvim/lua/andreas/plugins/trouble.lua
+++ .config/nvim/lua/andreas/plugins/trouble.lua
@@ -1,46 +1,37 @@
 return {
-	"folke/trouble.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	keys = {
-		{
-			"<leader>xx",
-			function()
-				require("trouble").toggle()
-			end,
-			desc = "trouble: toggle",
-		},
-		{
-			"<leader>xw",
-			function()
-				require("trouble").toggle("workspace_diagnostics")
-			end,
-			desc = "trouble: workspace diagnostics",
-		},
-		{
-			"<leader>xd",
-			function()
-				require("trouble").toggle("document_diagnostics")
-			end,
-			desc = "trouble: document diagnostics",
-		},
-		{
-			"<leader>xq",
-			function()
-				require("trouble").toggle("quickfix")
-			end,
-			desc = "trouble: quickfix",
-		},
-		{
-			"<leader>xl",
-			function()
-				require("trouble").toggle("loclist")
-			end,
-			desc = "trouble: loclist",
-		},
-	},
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-	},
+  "folke/trouble.nvim",
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
 }
