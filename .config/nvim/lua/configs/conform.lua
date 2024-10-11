ad2d1abd8f7de7405de1412feb791cd7b4e25792
Andreas Wågø Wilson <andreaswago.wilson@skatteetaten.no>
2024-10-11 14:50:09 +0200

    add shfmt

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -1,14 +1,12 @@
 local options = {
   formatters_by_ft = {
-    lua = { "stylua" },
     go = { "gofumpt" },
+    lua = { "stylua" },
+    sh = { "shfmt" },
     terraform = { "terraform_fmt" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
   },
 
   format_on_save = {
-    -- These options will be passed to conform.format()
     timeout_ms = 500,
     lsp_fallback = false,
   },
