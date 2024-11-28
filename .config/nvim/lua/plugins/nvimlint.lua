ba6541db4da027f0316596861168efb1c5a225e2
Andreas Wågø Wilson <andreasw@gmail.com>
2024-11-28 10:56:17 +0100

    auto install formatters from mason package registry

--- .config/nvim/lua/plugins/nvimlint.lua
+++ .config/nvim/lua/plugins/nvimlint.lua
@@ -12,6 +12,7 @@ return {
 			json = { "jsonlint" },
 			python = { "ruff" },
 			yaml = { "yamllint" },
+      terraform = {"tflint"},
 		}
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
