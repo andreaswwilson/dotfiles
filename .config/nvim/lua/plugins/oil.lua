73707ebf14129626a90efca2623b07978adfa9cd
Andreas Wågø Wilson <andreasw@gmail.com>
2024-11-29 18:35:25 +0100

    update vim

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,23 +1,25 @@
 return {
-	"stevearc/oil.nvim",
-	init = function()
-		vim.g.loaded_netrw = 1
-		vim.g.loaded_netrwPlugin = 1
-	end,
-	opts = {
-		default_file_explorer = true,
-		view_options = {
-			show_hidden = true,
-		},
-	},
-	dependencies = { { "echasnovski/mini.icons", opts = {} } },
-	keys = {
-		{
-			"<leader>e",
-			function()
-				require("oil").open()
-			end,
-			desc = "Open oil",
-		},
-	},
+  "stevearc/oil.nvim",
+  opts = {
+    default_file_explorer = true,
+    delete_to_trash = true,
+    skip_confirm_for_simple_edits = true,
+    view_options = {
+      natural_order = true,
+      show_hidden = true,
+    },
+    win_options = {
+      wrap = true,
+    }
+  },
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  keys = {
+    {
+      "<leader>e",
+      function()
+        require("oil").open()
+      end,
+      desc = "Open oil",
+    },
+  },
 }
