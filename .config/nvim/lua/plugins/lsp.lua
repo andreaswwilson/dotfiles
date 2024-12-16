8b8c0f15ba78d2bba015cc7758d8171c0b36ba00
Andreas Wågø Wilson <andreasw@gmail.com>
2024-12-16 18:00:22 +0100

    lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -6,6 +6,7 @@ return {
     dependencies = {
       'rafamadriz/friendly-snippets',
       "mikavilpas/blink-ripgrep.nvim",
+      { 'L3MON4D3/LuaSnip', version = 'v2.*' },
     },
 
     -- use a release tag to download pre-built binaries
@@ -22,22 +23,20 @@ return {
       keymap = { preset = 'enter' },
 
       appearance = {
-        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-        -- Useful for when your theme doesn't support blink.cmp
-        -- will be removed in a future release
-        use_nvim_cmp_as_default = true,
         -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
         -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = 'mono'
+        nerd_font_variant = 'normal'
       },
-
-      completion = {
-        list = {
-          selection = "auto_insert"
-        }
+      snippets = {
+        expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
+        active = function(filter)
+          if filter and filter.direction then
+            return require('luasnip').jumpable(filter.direction)
+          end
+          return require('luasnip').in_snippet()
+        end,
+        jump = function(direction) require('luasnip').jump(direction) end,
       },
-
-
       -- default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, via `opts_extend`
       sources = {
@@ -45,44 +44,6 @@ return {
         --
         -- optionally disable cmdline completions
         -- cmdline = {},
-      },
-      providers = {
-        providers = {
-          ripgrep = {
-            module = "blink-ripgrep",
-            name = "Ripgrep",
-            -- the options below are optional, some default values are shown
-            ---@module "blink-ripgrep"
-            ---@type blink-ripgrep.Options
-            opts = {
-              -- For many options, see `rg --help` for an exact description of
-              -- the values that ripgrep expects.
-
-              -- the minimum length of the current word to start searching
-              -- (if the word is shorter than this, the search will not start)
-              prefix_min_len = 3,
-
-              -- The number of lines to show around each match in the preview window.
-              -- For example, 5 means to show 5 lines before, then the match, and
-              -- another 5 lines after the match.
-              context_size = 5,
-
-              -- The maximum file size that ripgrep should include in its search.
-              -- Useful when your project contains large files that might cause
-              -- performance issues.
-              -- Examples: "1024" (bytes by default), "200K", "1M", "1G"
-              max_filesize = "1M",
-
-              -- (advanced) Any additional options you want to give to ripgrep.
-              -- See `rg -h` for a list of all available options. Might be
-              -- helpful in adjusting performance in specific situations.
-              -- If you have an idea for a default, please open an issue!
-              --
-              -- Not everything will work (obviously).
-              additional_rg_options = {}
-            },
-          },
-        },
       },
 
       -- experimental signature help support
