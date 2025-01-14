7fe3e0c10c88abae2e771791dd33dc8a501fe1b0
Andreas Wågø Wilson <andreasw@gmail.com>
2025-01-14 09:35:34 +0100

    add yanky

--- .config/nvim/lua/config/mappings.lua
+++ .config/nvim/lua/config/mappings.lua
@@ -7,8 +7,8 @@ map("i", "jk", "<ESC>")
 map({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 map("i", "<C-Space>", "<Space>", { silent = true })
 
-map("x", "p", [["_dP]]) -- Don't overwrite register when pasting
-map("n", "x", '"_x')    -- delete single char without copy into register
+-- map("x", "p", [["_dP]]) -- Don't overwrite register when pasting
+map("n", "x", '"_x') -- delete single char without copy into register
 map({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- Move Lines
@@ -33,49 +33,49 @@ map("v", ">", ">gv")
 
 local wk = require("which-key")
 wk.add({
-  { "<leader>f",   group = "fuzzy find" },
-  { "<leader>fg",  group = "git" },
-  { "<leader>b",   group = "buffer" },
-  { "<leader>x",   group = "trouble" },
-  -- Telescope
-  { "<leader>ff",  "<cmd>Telescope find_files<cr>",                desc = "files in cwd" },
-  { "<leader>fw",  "<cmd>Telescope live_grep<cr>",                 desc = "word in cwd" },
-  { "<leader>fW",  "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "word in buffer" },
-  { "<leader>fb",  "<cmd>Telescope buffers<cr>",                   desc = "buffers" },
-  { ",",           "<cmd>Telescope buffers<cr>",                   desc = "buffers" },
-  { "<leader>fh",  "<cmd>Telescope help_tags<cr>",                 desc = "buffers" },
-  { "<leader>fk",  "<cmd>Telescope keymaps<cr>",                   desc = "keymaps" },
-  { "<leader>fc",  "<cmd>Telescope commands<cr>",                  desc = "commands" },
-  { "<leader>fd",  "<cmd>Telescope diagnostics<cr>",               desc = "diagnostics" },
-  { "<leader>fr",  "<cmd>Telescope registers<cr>",                 desc = "registers" },
-  { "<leader>fgc", "<cmd>Telescope git_commits<cr>",               desc = "commits" },
-  { "<leader>fgb", "<cmd>Telescope git_bcommits<cr>",              desc = "buffer commits" },
-  { "<leader>fgs", "<cmd>Telescope git_stash<cr>",                 desc = "stash" },
+	{ "<leader>f", group = "fuzzy find" },
+	{ "<leader>fg", group = "git" },
+	{ "<leader>b", group = "buffer" },
+	{ "<leader>x", group = "trouble" },
+	-- Telescope
+	{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "files in cwd" },
+	{ "<leader>fw", "<cmd>Telescope live_grep<cr>", desc = "word in cwd" },
+	{ "<leader>fW", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "word in buffer" },
+	{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "buffers" },
+	{ ",", "<cmd>Telescope buffers<cr>", desc = "buffers" },
+	{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "buffers" },
+	{ "<leader>fk", "<cmd>Telescope keymaps<cr>", desc = "keymaps" },
+	{ "<leader>fc", "<cmd>Telescope commands<cr>", desc = "commands" },
+	{ "<leader>fd", "<cmd>Telescope diagnostics<cr>", desc = "diagnostics" },
+	{ "<leader>fr", "<cmd>Telescope registers<cr>", desc = "registers" },
+	{ "<leader>fgc", "<cmd>Telescope git_commits<cr>", desc = "commits" },
+	{ "<leader>fgb", "<cmd>Telescope git_bcommits<cr>", desc = "buffer commits" },
+	{ "<leader>fgs", "<cmd>Telescope git_stash<cr>", desc = "stash" },
 
-  -- Buffer mappings
-  {
-    "<leader>bd",
-    function()
-      require("mini.bufremove").delete()
-    end,
-    desc = "Delete buffer",
-  },
-  { "<leader>bo",        "<cmd>%bdelete|edit #<cr>",                   desc = "Close other buffers" },
-  { "<S-h>",             "<cmd>bprevious<cr>",                         desc = "Previous buffer" },
-  { "<S-l>",             "<cmd>bnext<cr>",                             desc = "Next buffer" },
+	-- Buffer mappings
+	{
+		"<leader>bd",
+		function()
+			require("mini.bufremove").delete()
+		end,
+		desc = "Delete buffer",
+	},
+	{ "<leader>bo", "<cmd>%bdelete|edit #<cr>", desc = "Close other buffers" },
+	{ "<S-h>", "<cmd>bprevious<cr>", desc = "Previous buffer" },
+	{ "<S-l>", "<cmd>bnext<cr>", desc = "Next buffer" },
 
-  -- smart splits
-  -- Moving between splits
-  { "<C-h>",             require("smart-splits").move_cursor_left,     desc = "Move cursor left" },
-  { "<C-j>",             require("smart-splits").move_cursor_down,     desc = "Move cursor down" },
-  { "<C-k>",             require("smart-splits").move_cursor_up,       desc = "Move cursor up" },
-  { "<C-l>",             require("smart-splits").move_cursor_right,    desc = "Move cursor right" },
-  { "<C-\\>",            require("smart-splits").move_cursor_previous, desc = "Move cursor to previous split" },
-  -- Swapping buffers between windows
-  { "<leader><leader>h", require("smart-splits").swap_buf_left,        desc = "Swap buffer left" },
-  { "<leader><leader>j", require("smart-splits").swap_buf_down,        desc = "Swap buffer down" },
-  { "<leader><leader>k", require("smart-splits").swap_buf_up,          desc = "Swap buffer up" },
-  { "<leader><leader>l", require("smart-splits").swap_buf_right,       desc = "Swap buffer right" },
+	-- smart splits
+	-- Moving between splits
+	{ "<C-h>", require("smart-splits").move_cursor_left, desc = "Move cursor left" },
+	{ "<C-j>", require("smart-splits").move_cursor_down, desc = "Move cursor down" },
+	{ "<C-k>", require("smart-splits").move_cursor_up, desc = "Move cursor up" },
+	{ "<C-l>", require("smart-splits").move_cursor_right, desc = "Move cursor right" },
+	{ "<C-\\>", require("smart-splits").move_cursor_previous, desc = "Move cursor to previous split" },
+	-- Swapping buffers between windows
+	{ "<leader><leader>h", require("smart-splits").swap_buf_left, desc = "Swap buffer left" },
+	{ "<leader><leader>j", require("smart-splits").swap_buf_down, desc = "Swap buffer down" },
+	{ "<leader><leader>k", require("smart-splits").swap_buf_up, desc = "Swap buffer up" },
+	{ "<leader><leader>l", require("smart-splits").swap_buf_right, desc = "Swap buffer right" },
 
-  { "<leader>d",         "<cmd>lua vim.diagnostic.open_float()<cr>",   desc = "Diagnostic open float" },
+	{ "<leader>d", "<cmd>lua vim.diagnostic.open_float()<cr>", desc = "Diagnostic open float" },
 })
