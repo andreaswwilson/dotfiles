a66cf9c1b624389a220cff7704a5c872dea0eea9
Andreas Wågø Wilson <andreasw@gmail.com>
2025-01-17 18:30:07 +0100

    wezterm

---
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,49 @@
+return {
+	"epwalsh/obsidian.nvim",
+	version = "*", -- recommended, use latest release instead of latest commit
+	lazy = true,
+	ft = "markdown",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	opts = {
+		ui = { enable = false },
+		completion = {
+			-- Set to false to disable completion.
+			nvim_cmp = true,
+			-- Trigger completion at 2 chars.
+			min_chars = 2,
+		},
+		-- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
+		-- way then set 'mappings = {}'.
+		mappings = {
+			-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+			["gf"] = {
+				action = function()
+					return require("obsidian").util.gf_passthrough()
+				end,
+				opts = { noremap = false, expr = true, buffer = true },
+			},
+			-- Toggle check-boxes.
+			["<leader>cc"] = {
+				action = function()
+					return require("obsidian").util.toggle_checkbox()
+				end,
+				opts = { buffer = true },
+			},
+			-- Smart action depending on context, either follow link or toggle checkbox.
+			["<cr>"] = {
+				action = function()
+					return require("obsidian").util.smart_action()
+				end,
+				opts = { buffer = true, expr = true },
+			},
+		},
+		workspaces = {
+			{
+				name = "andreas",
+				path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/andreas",
+			},
+		},
+	},
+}
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,172 +5,169 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- smart-splits.nvim
 -- if you are *NOT* lazy-loading smart-splits.nvim (recommended)
 local function is_vim(pane)
-  -- this is set by the plugin, and unset on ExitPre in Neovim
-  return pane:get_user_vars().IS_NVIM == "true"
+	-- this is set by the plugin, and unset on ExitPre in Neovim
+	return pane:get_user_vars().IS_NVIM == "true"
 end
 local direction_keys = {
-  h = "Left",
-  j = "Down",
-  k = "Up",
-  l = "Right",
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
 }
 
 local function split_nav(resize_or_move, key)
-  return {
-    key = key,
-    mods = resize_or_move == "resize" and "META" or "CTRL",
-    action = wezterm.action_callback(function(win, pane)
-      if is_vim(pane) then
-        -- pass the keys through to vim/nvim
-        win:perform_action({
-          SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
-        }, pane)
-      else
-        if resize_or_move == "resize" then
-          win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
-        else
-          win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
-        end
-      end
-    end),
-  }
+	return {
+		key = key,
+		mods = resize_or_move == "resize" and "META" or "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if is_vim(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+				}, pane)
+			else
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
 end
 config.color_scheme = "Tokyo Night"
 config.font_size = 14
 
 -- https://wezfurlong.org/wezterm/config/lua/keyassignment/SwitchToWorkspace.html
-wezterm.on("update-right-status", function(window, pane)
-  window:set_right_status(window:active_workspace())
+
+-- wezterm.on("update-right-status", function(window, pane)
+-- 	window:set_right_status(window:active_workspace())
+-- end)
+-- wezterm.on("update-status", function(window, pane)
+-- 	window:set_left_status(window:active_workspace() .. ":")
+-- end)
+wezterm.on("update-status", function(window, pane)
+	local workspace = window:active_workspace()
+	local padded_workspace = string.format("%-15s", " " .. workspace)
+	window:set_left_status(wezterm.format({
+		{ Background = { Color = "#c0caf5" } },
+		{ Foreground = { Color = "#414868" } },
+		{ Text = padded_workspace },
+	}))
 end)
+config.window_decorations = "RESIZE"
+config.use_fancy_tab_bar = false
 
 config.keys = {
-  -- Splitting
-  {
-    mods = "CMD|SHIFT",
-    key = "v",
-    action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-  },
+	-- Splitting
+	{
+		mods = "CMD|SHIFT",
+		key = "v",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
 
-  {
-    mods = "CMD|SHIFT",
-    key = "h",
-    action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-  },
-  -- show the pane selection mode, but have it swap the active and selected panes
-  {
-    mods = "CMD|SHIFT",
-    key = "s",
-    action = wezterm.action.PaneSelect({
-      mode = "SwapWithActive",
-    }),
-  },
-  -- Prompt for a name to use for a new workspace and switch to it.
-  {
-    key = "w",
-    mods = "CMD|SHIFT",
-    action = wezterm.action.PromptInputLine({
-      description = wezterm.format({
-        { Attribute = { Intensity = "Bold" } },
-        { Foreground = { AnsiColor = "Fuchsia" } },
-        { Text = "Enter name for new workspace" },
-      }),
-      action = wezterm.action_callback(function(window, pane, line)
-        -- line will be `nil` if they hit escape without entering anything
-        -- An empty string if they just hit enter
-        -- Or the actual line of text they wrote
-        if line then
-          window:perform_action(
-            wezterm.action.SwitchToWorkspace({
-              name = line,
-            }),
-            pane
-          )
-        end
-      end),
-    }),
-  },
-  -- Show the launcher in fuzzy selection mode and have it list all workspaces
-  -- and allow activating one.
-  {
-    key = "e",
-    mods = "CMD|SHIFT",
-    action = wezterm.action.ShowLauncherArgs({
-      flags = "FUZZY|WORKSPACES",
-    }),
-  },
-  -- Switch workspace
-  { key = "n", mods = "CMD|SHIFT", action = wezterm.action.SwitchWorkspaceRelative(1) },
-  { key = "p", mods = "CMD|SHIFT", action = wezterm.action.SwitchWorkspaceRelative(-1) },
-  -- Rename workspace
-  {
-    key = "r",
-    mods = "CMD|SHIFT",
-    action = wezterm.action.PromptInputLine({
-      description = wezterm.format({
-        { Attribute = { Intensity = "Bold" } },
-        { Foreground = { AnsiColor = "Fuchsia" } },
-        { Text = "Rename current workspace" },
-      }),
-      action = wezterm.action_callback(function(window, pane, line)
-        -- line will be `nil` if they hit escape without entering anything
-        -- An empty string if they just hit enter
-        -- Or the actual line of text they wrote
-        if line then
-          window:perform_action(wezterm.mux.rename_workspace(wezterm.mux.get_active_workspace(), line), pane)
-        end
-      end),
-    }),
-  },
-  -- change tabs
-  {
-    key = "h",
-    mods = "SUPER",
-    action = wezterm.action.ActivateTabRelative(-1),
-  },
-  {
-    key = "l",
-    mods = "SUPER",
-    action = wezterm.action.ActivateTabRelative(1),
-  },
-  {
-    key = "w",
-    mods = "CMD",
-    action = wezterm.action.CloseCurrentPane({ confirm = false }),
-  },
-  {
-    key = "F12",
-    action = wezterm.action_callback(function(_, pane)
-      local tab = pane:tab()
-      local panes = tab:panes_with_info()
-      if #panes == 1 then
-        local new_pane = pane:split({
-          direction = "Right",
-        })
-      elseif not panes[1].is_zoomed then
-        panes[1].pane:activate()
-        tab:set_zoomed(true)
-      elseif panes[1].is_zoomed then
-        tab:set_zoomed(false)
-        panes[2].pane:activate()
-      end
-    end),
-  },
-  -- smart-splits.nvim
-  split_nav("move", "h"),
-  split_nav("move", "j"),
-  split_nav("move", "k"),
-  split_nav("move", "l"),
-  -- resize panes
-  split_nav("resize", "h"),
-  split_nav("resize", "j"),
-  split_nav("resize", "k"),
-  split_nav("resize", "l"),
+	{
+		mods = "CMD|SHIFT",
+		key = "h",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	-- show the pane selection mode, but have it swap the active and selected panes
+	{
+		mods = "CMD|SHIFT",
+		key = "s",
+		action = wezterm.action.PaneSelect({
+			mode = "SwapWithActive",
+		}),
+	},
+	-- Prompt for a name to use for a new workspace and switch to it.
+	{
+		key = "w",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.PromptInputLine({
+			description = wezterm.format({
+				{ Attribute = { Intensity = "Bold" } },
+				{ Foreground = { AnsiColor = "Fuchsia" } },
+				{ Text = "Enter name for new workspace" },
+			}),
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:perform_action(
+						wezterm.action.SwitchToWorkspace({
+							name = line,
+						}),
+						pane
+					)
+				end
+			end),
+		}),
+	},
+	-- Show the launcher in fuzzy selection mode and have it list all workspaces
+	-- and allow activating one.
+	{
+		key = "e",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.ShowLauncherArgs({
+			flags = "FUZZY|WORKSPACES",
+		}),
+	},
+	-- Switch workspace
+	{ key = "n", mods = "CMD|SHIFT", action = wezterm.action.SwitchWorkspaceRelative(1) },
+	{ key = "p", mods = "CMD|SHIFT", action = wezterm.action.SwitchWorkspaceRelative(-1) },
+	-- Rename workspace
+	{
+		key = "r",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.PromptInputLine({
+			description = wezterm.format({
+				{ Attribute = { Intensity = "Bold" } },
+				{ Foreground = { AnsiColor = "Fuchsia" } },
+				{ Text = "Rename current workspace" },
+			}),
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:perform_action(wezterm.mux.rename_workspace(wezterm.mux.get_active_workspace(), line), pane)
+				end
+			end),
+		}),
+	},
+	-- change tabs
+	{
+		key = "h",
+		mods = "SUPER",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		key = "l",
+		mods = "SUPER",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		key = "w",
+		mods = "CMD",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
+	-- smart-splits.nvim
+	split_nav("move", "h"),
+	split_nav("move", "j"),
+	split_nav("move", "k"),
+	split_nav("move", "l"),
+	-- resize panes
+	split_nav("resize", "h"),
+	split_nav("resize", "j"),
+	split_nav("resize", "k"),
+	split_nav("resize", "l"),
 }
 
 return config
