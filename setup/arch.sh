c4314a100bc743f90dc2e566a2cf44e6f0a31a13
Andreas Wågø Wilson <andreas.wilson@vimsa.com>
2025-10-28 10:26:02 +0100

    add jq

--- setup/arch.sh
+++ setup/arch.sh
@@ -6,8 +6,7 @@ sudo pacman -S --noconfirm --needed \
   firefox git neovim stow go ghostty wl-clipboard hyprlock pamixer \
   waybar rofi-wayland github-cli zsh fzf thunar pavucontrol ttf-jetbrains-mono-nerd \
   zoxide atuin starship lsd luarocks lazygit dunst otf-font-awesome npm brightnessctl \
-  blueman bat base-devel opentofu tmux 
-
+  blueman bat base-devel opentofu tmux jq
 
 # ----------------------------------------------------------------------
 
@@ -22,4 +21,3 @@ if pacman -Q kitty &>/dev/null; then
   echo "Removing kitty..."
   sudo pacman -Rns --noconfirm kitty
 fi
-
